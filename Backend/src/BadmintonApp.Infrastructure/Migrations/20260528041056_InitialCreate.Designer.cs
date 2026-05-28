8caaf45cc7d54275236ce28c9fd5c38ffd440fe9
DinhTuanKhoiTB01696 <khoidttb01696@Gmail.com>
2026-05-28 11:18:04 +0700

    fix: Them headers COOP cho Vite server ho tro Google Login va cap nhat DB migration

--- Backend/src/BadmintonApp.Infrastructure/Migrations/20260528041056_InitialCreate.Designer.cs
+++ Backend/src/BadmintonApp.Infrastructure/Migrations/20260528041056_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BadmintonApp.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260527160942_InitialCreate")]
+    [Migration("20260528041056_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
